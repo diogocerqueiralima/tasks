ee8a90ae3357e2a9e2553595c921d99104f13c67
diogodelima <diogocerqueiradeelima@gmail.com>
2025-01-10 18:57:41 +0000

    feat: add request to userinfo endpoint to retrieve userId and put on header 'User-Id'

---
+++ gateway/src/main/kotlin/com/github/diogodelima/gateway/config/WebClientConfig.kt
@@ -0,0 +1,13 @@
+package com.github.diogodelima.gateway.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class WebClientConfig {
+
+    @Bean
+    fun webClient() = WebClient.builder().build()
+
+}
