b00ba4bb9435bfe62bda19e2b941db1650048328
diogodelima <diogocerqueiradeelima@gmail.com>
2025-01-17 10:25:31 +0000

    fix: cors config

--- gateway/src/main/kotlin/com/github/diogodelima/gateway/config/SecurityConfig.kt
+++ gateway/src/main/kotlin/com/github/diogodelima/gateway/config/SecurityConfig.kt
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.cors.reactive.CorsWebFilter
+import java.util.*
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,6 +18,16 @@ class SecurityConfig {
     @Bean
     fun defaultSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
         http
+            .cors {
+                it.configurationSource {
+                    val corsConfig = CorsConfiguration()
+                    corsConfig.allowedOrigins = listOf("http://localhost:3000")
+                    corsConfig.allowedMethods = listOf("*")
+                    corsConfig.allowedHeaders = listOf("*")
+                    corsConfig.allowCredentials = true
+                    corsConfig
+                }
+            }
             .csrf { it.disable() }
             .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
             .authorizeExchange { exchange ->
